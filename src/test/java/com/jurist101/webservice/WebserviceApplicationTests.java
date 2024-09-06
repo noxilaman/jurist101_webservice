96a24721132abb0bdbc53692cc4588ac0f77e76c
parinya.k <parinya.k@lannaagro.com>
2024-09-06 12:07:51 +0700

    Base

---
+++ src/test/java/com/jurist101/webservice/WebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jurist101.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
